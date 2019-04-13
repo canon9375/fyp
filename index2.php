ff664949164a3f4b220836942bf38fdd88692824
canon9375 <36266115+canon9375@users.noreply.github.com>
2019-04-13 20:40:04 +0800

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html>
+<title></title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.w3-sidebar a {font-family: "Roboto", sans-serif}
+body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
+</style>
+<body class="w3-content" style="max-width:1200px">
+
+<!-- Sidebar/menu -->
+<nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
+  <div class="w3-container w3-display-container w3-padding-16">
+    <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
+    <h3 class="w3-wide"><b>Air Quality Prediction</b></h3>
+  </div>
+  <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
+	  <a onclick="myAccFunc()" href="javascript:void(0)" class="w3-button w3-block w3-white w3-left-align" id="myBtn">
+      Map <i class="fa fa-caret-down"></i>
+    </a>
+    <div id="demoAcc" class="w3-bar-block w3-hide w3-padding-large w3-medium">
+      
+      <a href="./MainPage/mapDisplay.php" class="w3-bar-item w3-button" target="con">Random Forest</a>
+      <a href="#" class="w3-bar-item w3-button" target="con">Time Series(ARIMA)</a>
+      <a href="#" class="w3-bar-item w3-button"target="con">ANN</a>
+      <a href="#" class="w3-bar-item w3-button"target="con">Mathematic</a>
+    </div>
+    <a href="./callAPI/ARIMA.html" class="w3-bar-item w3-button" target="con">Get Data</a>
+
+      <a href="#" class="w3-bar-item w3-button" onClick="show3()">Chart<i class="fa fa-caret-down"></i></a>
+      <div id="demoAcc3" class="w3-bar-block w3-hide w3-padding-large w3-medium">
+          <a href="./chart/chart.php" class="w3-bar-item w3-button" target="con">Random Forest</a>
+          <a href="./TestPlan/ARIMAMain.html" class="w3-bar-item w3-button" target="con">Time Series(ARIMA)</a>
+          <a href="./TestPlan/MLP.html" class="w3-bar-item w3-button" target="con">Multiple Linear Regression</a>
+      </div>
+
+
+
+    <a href="#" class="w3-bar-item w3-button" onClick="show3()">Test Plan<i class="fa fa-caret-down"></i></a>
+	  <div id="demoAcc3" class="w3-bar-block w3-hide w3-padding-large w3-medium">
+      <a href="./TestPlan/MLP.html" class="w3-bar-item w3-button" target="con">Random Forest</a>
+      <a href="./TestPlan/ARIMAMain.html" class="w3-bar-item w3-button" target="con">Time Series(ARIMA)</a>
+      <a href="./TestPlan/MLP.html" class="w3-bar-item w3-button" target="con">Multiple Linear Regression</a>
+    </div>
+	  
+    <a href="#" class="w3-bar-item w3-button" onClick="show2()" >Explanation<i class="fa fa-caret-down"></i></a>
+	<div id="demoAcc2" class="w3-bar-block w3-hide w3-padding-large w3-medium">
+      <a href="./Explaination/rf.html" class="w3-bar-item w3-button" target="con">Random Forest</a>
+      <a href="./Explaination/ARIMA.html" class="w3-bar-item w3-button" target="con">Time Series(ARIMA)</a>
+      <a href="./Explaination/math.html" class="w3-bar-item w3-button" target="con">Mathematic</a>
+    </div>
+    <a href="#" class="w3-bar-item w3-button" onClick="show4()">Past Data<i class="fa fa-caret-down"></i></a>
+      <div id="demoAcc4" class="w3-bar-block w3-hide w3-padding-large w3-medium">
+          <a href="https://www.hko.gov.hk/m/menu_uc.htm?" class="w3-bar-item w3-button" target="con">weather</a>
+          <a href="https://www.epd.gov.hk/epd/tc_chi/environmentinhk/air/data/air_data.html" class="w3-bar-item w3-button" target="con">Pollutant Concentration</a>
+
+      </div>
+    <a href="./getAQHI/byGPS.html" target="con" class="w3-bar-item w3-button">Get AQHI</a>
+  </div>
+
+</nav>
+<!--<iframe src="./MainPage/mapDisplay.php" style="border: 1px solid black ;  width: 100%;height: 1000px;" name="con" frameborder="0"></iframe>-->
+<iframe  style="display: inline-block; margin-top: 0px; padding-left: 15%; width: 100%;height: 1000px;"  name="con" src="./MainPage/mapDisplay.php" frameborder="0">
+<!--	-->
+<!--	</iframe>-->
+<!--	-->
+	
+
+<!-- Top menu on small screens -->
+<header class="w3-bar w3-top w3-hide-large w3-black w3-xlarge">
+  <div class="w3-bar-item w3-padding-24 w3-wide">Air Quality Predict</div>
+  <a href="javascript:void(0)" class="w3-bar-item w3-button w3-padding-24 w3-right" onclick="w3_open()"><i class="fa fa-bars"></i></a>
+</header>
+
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-main" style="margin-left:250px">
+
+  <!-- Push down content on small screens -->
+  <div class="w3-hide-large" style="margin-top:83px"></div>
+  
+  <!-- Top header -->
+  <header class="w3-container w3-xlarge">
+
+
+<!-- Newsletter Modal -->
+<div id="newsletter" class="w3-modal">
+  <div class="w3-modal-content w3-animate-zoom" style="padding:32px">
+    <div class="w3-container w3-white w3-center">
+      <i onclick="document.getElementById('newsletter').style.display='none'" class="fa fa-remove w3-right w3-button w3-transparent w3-xxlarge"></i>
+      <h2 class="w3-wide">NEWSLETTER</h2>
+      <p>Join our mailing list to receive updates on new arrivals and special offers.</p>
+      <p><input class="w3-input w3-border" type="text" placeholder="Enter e-mail"></p>
+      <button type="button" class="w3-button w3-padding-large w3-red w3-margin-bottom" onclick="document.getElementById('newsletter').style.display='none'">Subscribe</button>
+    </div>
+  </div>
+</div>
+
+<script>
+// Accordion 
+function myAccFunc() {
+  var x = document.getElementById("demoAcc");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+function show2() {
+  var x = document.getElementById("demoAcc2");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+function show3() {
+  var x = document.getElementById("demoAcc3");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+function show4() {
+    var x = document.getElementById("demoAcc4");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+// Click on the "Jeans" link on page load to open the accordion for demo purposes
+document.getElementById("myBtn").click();
+
+
+// Open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+</script>
+
+</body>
+</html>
